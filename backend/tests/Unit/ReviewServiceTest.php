52e03196801680499d5c5558fc13ab4fa02809b5
gordo <gordo@hakim.local>
2026-05-04 18:36:26 +0100

    Add Unit test for ReviewService

---
+++ backend/tests/Unit/ReviewServiceTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\Services\ReviewService;
+use Tests\TestCase;
+
+class ReviewServiceTest extends TestCase
+{
+    public function test_calculate_average_rating()
+    {
+        $this->assertTrue(true);
+    }
+}
