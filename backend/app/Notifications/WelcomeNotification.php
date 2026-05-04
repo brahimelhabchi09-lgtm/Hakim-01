82d6ab0b5d34584e7b1052e9ea22ed8fb0152682
gordo <gordo@hakim.local>
2026-05-04 18:31:11 +0100

    Add WelcomeNotification for new users

---
+++ backend/app/Notifications/WelcomeNotification.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Notifications\Notification;
+use Illuminate\Notifications\Messages\MailMessage;
+
+class WelcomeNotification extends Notification
+{
+    use Queueable;
+
+    public function via($notifiable): array
+    {
+        return ['mail'];
+    }
+
+    public function toMail($notifiable): MailMessage
+    {
+        return (new MailMessage)
+            ->subject('Welcome to WolfStore!')
+            ->line('Thank you for creating an account.')
+            ->action('Start Shopping', url('/'));
+    }
+}
