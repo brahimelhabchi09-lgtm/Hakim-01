f09b6fb79081f90c3bf71276519a7c42c4f36099
gordo <gordo@hakim.local>
2026-05-04 18:39:37 +0100

    Add Feature test for logout endpoint

---
+++ backend/tests/Feature/LogoutTest.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class LogoutTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function test_user_can_logout()
+    {
+        $user = User::factory()->create();
+        $response = $this->actingAs($user)->postJson('/api/logout');
+        $response->assertOk();
+    }
+}
