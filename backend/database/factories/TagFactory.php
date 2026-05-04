af4aaf25cb6ad68c402a2a65e59d0169e1431760
gordo <gordo@hakim.local>
2026-05-04 18:26:16 +0100

    Add TagFactory

---
+++ backend/database/factories/TagFactory.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Tag;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
+
+class TagFactory extends Factory
+{
+    protected $model = Tag::class;
+
+    public function definition(): array
+    {
+        $nom = fake()->word();
+        return [
+            'nom' => $nom,
+            'slug' => Str::slug($nom),
+        ];
+    }
+}
