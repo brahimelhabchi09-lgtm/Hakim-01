fd516cc1bfb77332fa3aaf9404f8cec473d6fbce
gordo <gordo@hakim.local>
2026-05-04 18:32:15 +0100

    Update config/view.php blade compilation path

--- backend/config/view.php
+++ backend/config/view.php
@@ -1,11 +1,6 @@
 <?php
 
 return [
-    'paths' => [
-        resource_path('views'),
-    ],
-    'compiled' => env(
-        'VIEW_COMPILED_PATH',
-        realpath(storage_path('framework/views'))
-    ),
+    'paths' => [resource_path('views')],
+    'compiled' => env('VIEW_COMPILED_PATH', storage_path('framework/views')),
 ];
