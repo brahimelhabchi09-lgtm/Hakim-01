3b0deeba11cd78629472a7c393af77fe1474dc4d
gordo <gordo@hakim.local>
2026-05-04 18:21:35 +0100

    Add ui/ProductCardSkeleton.vue loading placeholder

---
+++ frontend/src/components/ui/ProductCardSkeleton.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="card animate-pulse">
+    <div class="aspect-square bg-gray-200" />
+    <div class="p-4 space-y-3">
+      <div class="h-4 bg-gray-200 rounded w-3/4" />
+      <div class="h-3 bg-gray-200 rounded w-1/2" />
+      <div class="h-4 bg-gray-200 rounded w-1/3" />
+      <div class="h-8 bg-gray-200 rounded w-full" />
+    </div>
+  </div>
+</template>
