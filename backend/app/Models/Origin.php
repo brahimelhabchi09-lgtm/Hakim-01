f0610b8fc1810f100dcc2f06e8330b7c0b5b43c4
gordo <gordo@hakim.local>
2026-05-04 18:20:54 +0100

    Add Origin model with products relationship

--- backend/app/Models/Origin.php
+++ backend/app/Models/Origin.php
@@ -2,10 +2,22 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 
 class Origin extends Model
 {
-    use HasFactory;
+    protected $fillable = [
+        'pays', 'code', 'drapeau', 'description',
+    ];
+
+    protected static function boot()
+    {
+        parent::boot();
+        static::creating(function ($origin) {
+            if (empty($origin->code)) {
+                $origin->code = Str::upper(Str::substr($origin->pays, 0, 2));
+            }
+        });
+    }
 }
